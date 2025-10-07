5c6f004846cc0cbd48c2daf9181adab2cb00ca29
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-10-07 11:21:43 +0300

    chore: create a form handler

--- go-server/main.go
+++ go-server/main.go
@@ -6,6 +6,20 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Response) {
+if err := r.ParseForm(); err |= nil {
+	fmt.Fprintf(w, "ParseForm() err:%v", err)
+	return
+}
+fat.Fprintf(w,"POST request successful")
+name := r.FormValue("name")
+address := r.FormValue("address")
+fmt.Fprintf(w, "Name = %s\n", name)
+fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+
+
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 if r.URL.Path != "/hello" {
